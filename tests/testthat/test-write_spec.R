c0ac25b64fe626f4106cb8ed8e2cc3d5b040ebcf
Tom Elliott <tomelliottnz@gmail.com>
2021-11-17 13:48:31 +1300

    Add tests for write_spec

--- tests/testthat/test-write_spec.R
+++ tests/testthat/test-write_spec.R
@@ -21,3 +21,31 @@ test_that("Survey design file written correctly", {
         ignore_attr = TRUE
     )
 })
+
+test_that("Survey design for stratified, unclustered data", {
+    data <- apistrat
+    des <- survey::svydesign(ids = ~1, strata = ~stype,
+        fpc = ~fpc, weights = ~pw, data = data)
+
+    f <- tempfile(fileext = ".svydesign")
+    on.exit(unlink(f))
+
+    write_spec(des, f)
+
+    expect_equal(
+        readLines(f),
+        c("strata = \"stype\"", "fpc = \"fpc\"", "weights = \"pw\"", "type = \"survey\"")
+    )
+
+    expect_equal(
+        import_survey(f, data)$design,
+        des,
+        ignore_attr = TRUE
+    )
+})
+
+test_that("Errors returned if object not supported", {
+    expect_error(
+        write_spec(iris)
+    )
+})
